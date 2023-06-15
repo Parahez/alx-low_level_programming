bb62652e40ef9f844bf8048a5f93b67ce44da533
Parahez <genetshibru32@gmail.com>
2023-06-15 14:51:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * puts - "Programming is like building a multilingual puzzle, followed by a new line."
- *
+ * puts - "Programming is like building a multilingual puzzle, followed by a new line.
  * Return: Always 0 (Success),
  */
 int main(void)
