b54bd8779f91d8f2c6fadbbb7494a2e45d468d51
Parahez <genetshibru32@gmail.com>
2023-06-15 13:58:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,14 @@
 
 /**
  * main - Entry point
- *
  * puts - "Programming is like building a multilingual puzzle, followed by a new line."
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
        
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
        
        	return (0);
 
