938e429e0dfe22f5f9a882b64742387c7164d929
Parahez <genetshibru32@gmail.com>
2023-06-26 13:55:31 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Write a function that prints a string, followed by a new line, to
+ * stdout.
+ *
+ * @str: This is my input string
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
