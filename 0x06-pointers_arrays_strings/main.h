acfcb7161ce231c9d72c52469d474aed7c0429f6
Parahez <genetshibru32@gmail.com>
2023-06-29 10:25:07 -0700

    task-2_fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
+char *_strncpy(char *dest, char *src, int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
